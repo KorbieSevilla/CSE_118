c82c0fe42fdb5b916be1bbf1ae66d176d0d09a8a
Yong Lu <yonglu777@gmail.com>
2019-11-06 11:12:08 -0800

    Merged the two files

--- RentADriveMobile2/app/src/main/java/com/zybooks/rentadrivemobile2/MainActivity.java
+++ RentADriveMobile2/app/src/main/java/com/zybooks/rentadrivemobile2/MainActivity.java
@@ -1,16 +1,66 @@
 package com.zybooks.rentadrivemobile2;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
+    EditText email, password;
+    Button login, register;
+    FirebaseAuth fAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        startActivity(new Intent(this, Register.class));
+
+        email = findViewById(R.id.loginUser);
+        password = findViewById(R.id.loginPass);
+        login = findViewById(R.id.login);
+        register = findViewById(R.id.register);
+
+        fAuth = FirebaseAuth.getInstance();
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String pw = password.getText().toString();
+                String uEmail = email.getText().toString();
+
+                if(uEmail.isEmpty()){
+                    email.setError("Email required");
+                    return;
+                }
+                if(pw.length() < 6){
+                    password.setError("Enter a password of length at least 6 characters");
+                    return;
+                }
+
+                fAuth.signInWithEmailAndPassword(uEmail,pw).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        Toast.makeText(MainActivity.this, "Login Succeeded", Toast.LENGTH_LONG).show();
+                    }
+                });
+            }
+        });
+
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, Register.class));
+            }
+        });
     }
 }
--- RentADriveMobile2/app/src/main/java/com/zybooks/rentadrivemobile2/MapsActivity.java
+++ RentADriveMobile2/app/src/main/java/com/zybooks/rentadrivemobile2/MapsActivity.java
@@ -13,7 +13,7 @@ import com.google.android.gms.maps.model.MarkerOptions;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
-    private GoogleMap mMap;
+    private GoogleMap mMap, zoomLevel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,10 +38,12 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
+        float zoomLevel = 14.0f;
 
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+
+        // Add a marker in Santa Cruz and move the camera
+        LatLng santaCruz = new LatLng(36.9741 , -122.0308);
+        mMap.addMarker(new MarkerOptions().position(santaCruz).title("Marker in Santa Cruz"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(santaCruz, zoomLevel));
     }
 }
