36aec68d3389f7cb007e3a3aa307e1f016c13566
KorbieSevilla <korbiesevilla@gmail.com>
2019-12-04 18:25:36 -0800

    remove a line
    redundant line

--- RentADriveMobile2/app/src/main/java/com/zybooks/rentadrivemobile2/ui/home/HomeFragment.java
+++ RentADriveMobile2/app/src/main/java/com/zybooks/rentadrivemobile2/ui/home/HomeFragment.java
@@ -118,7 +118,6 @@ public class HomeFragment extends Fragment implements OnMapReadyCallback {
                         );
                     }
                 }
-                    //mMap.moveCamera(CameraUpdateFactory.newLatLngZoom( ,zoomLevel));
             }
 
 
